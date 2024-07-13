441d99828055cbe4b8bcddd32f5acc080a9b5fd9
phetploy <st.phetploy@gmail.com>
2024-07-14 02:32:03 +0700

    Add test for calculate allowance

--- handlers/tax_test.go
+++ handlers/tax_test.go
@@ -2,10 +2,11 @@ package handlers
 
 import (
 	"testing"
-)
 
-func TestCalculateTaxAmount(t *testing.T) {
-}
+	"github.com/KKGo-Software-engineering/assessment-tax/config"
+	"github.com/KKGo-Software-engineering/assessment-tax/models"
+	"github.com/go-playground/assert/v2"
+)
 
 func TestCalculateProgressiveRate(t *testing.T) {
 	tests := []struct {
@@ -33,4 +34,96 @@ func TestCalculateProgressiveRate(t *testing.T) {
 			t.Errorf("For netIncome %.2f, tax rate expected = %.2f but got = %.2f", test.netIncome, test.expected, result)
 		}
 	}
+}
+
+// func setupTestDB() *gorm.DB {
+// 	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+// 	if err != nil {
+// 		panic(fmt.Sprintf("Failed to open database: %v", err))
+// 	}
+// 	db.AutoMigrate(&models.TaxCalculation{}, &models.Allowance{}, &models.AdminSetting{})
+
+// 	// Insert admin settings
+// 	adminSetting := models.AdminSetting{
+// 		KReceiptDeduction: 50000.0,
+// 		PersonalDeduction: 60000.0,
+// 	}
+// 	db.Create(&adminSetting)
+
+// 	// config.ConnectDB(db) // Set the mock database to the global config
+
+// 	return db
+// }
+
+func TestCalculateAllowance(t *testing.T) {
+
+	t.Run("test maximum allowances", func(t *testing.T) {
+		// I'll be back to mock DB
+		config.ConnectDB()
+
+		allowances := []models.Allowance{
+			{AllowanceType: "donation", Amount: 120000.0},
+			{AllowanceType: "k-receipt", Amount: 70000.0},
+			{AllowanceType: "personal", Amount: 50000.0},
+			{AllowanceType: "", Amount: 20000.0},
+		}
+
+		expected := 100000.0 + 50000.0 + 0.0
+
+		got := calculateAllowance(allowances)
+
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("test normal allowances", func(t *testing.T) {
+		// I'll be back to mock DB
+		config.ConnectDB()
+
+		allowances := []models.Allowance{
+			{AllowanceType: "donation", Amount: 90000.0},
+			{AllowanceType: "k-receipt", Amount: 40000.0},
+			{AllowanceType: "personal", Amount: 0.0},
+		}
+
+		expected := 90000.0 + 40000.0
+
+		got := calculateAllowance(allowances)
+
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("test normal allowances", func(t *testing.T) {
+		// I'll be back to mock DB
+		config.ConnectDB()
+
+		allowances := []models.Allowance{
+			{AllowanceType: "donation", Amount: 90000.0},
+			{AllowanceType: "k-receipt", Amount: 40000.0},
+			{AllowanceType: "personal", Amount: 70000.0},
+		}
+
+		expected := 90000.0 + 40000.0 + 70000.0
+
+		got := calculateAllowance(allowances)
+
+		assert.Equal(t, expected, got)
+	})
+
+	t.Run("non calculate personal allowance with less than 60000", func(t *testing.T) {
+		// I'll be back to mock DB
+		config.ConnectDB()
+
+		allowances := []models.Allowance{
+			{AllowanceType: "donation", Amount: 90000.0},
+			{AllowanceType: "k-receipt", Amount: 40000.0},
+			{AllowanceType: "personal", Amount: 50000.0},
+		}
+
+		expected := 90000.0 + 40000.0 + 0.0
+
+		got := calculateAllowance(allowances)
+
+		assert.Equal(t, expected, got)
+	})
+
 }
